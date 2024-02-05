0f17861205d6496ba0f60635dda830a67d854bb8
Mica Liste <micaelaliste@gmail.com>
2024-02-05 20:04:19 -0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+require("dotenv").config()
 // const syncApiDb= require("./src/controllers/apiDB.js")
 
 conn.sync( false).then(async() => {
 // await syncApiDb()
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log('Server listening on port', process.env.PORT);
 })
 }).catch(error => console.error(error))
